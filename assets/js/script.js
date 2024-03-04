cb74e3c9a31e51c4ce5926af6e07f98d67c16529
motosasaki <64741202+motosasaki@users.noreply.github.com>
2024-03-04 18:42:07 +0100

    thesis

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+// if you use jQuery, you need to start your .js file with this.
+// otherwise you can delete everything here.
+$(document).ready(function() {
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+}
+
+});
