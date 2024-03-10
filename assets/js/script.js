ffaab1d7e171f1e2a956489a2a41bb86be6ea23c
motosasaki <64741202+motosasaki@users.noreply.github.com>
2024-03-10 13:57:11 +0100

    d

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,3 +17,12 @@ function closeNav() {
 $( function() {
   $( ".draggable" ).draggable();
 });
+
+window.addEventListener('scroll', function() {
+  var element = document.getElementById('h1 ');
+  if (window.scrollY > 40) { // Change 100 to the scroll position at which you want the color to change
+    element.style.backgroundColor = 'whitesmoke'; // Change to the desired color
+  } else {
+    element.style.backgroundColor = 'yellow'; // Change back to the original color
+  }
+});
